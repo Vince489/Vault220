c16c3ef038428d76dc4a2d30be8239189d887c85
Vince489 <vincentcornelius489@gmail.com>
2025-01-04 07:57:07 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,css,js}",   // Includes all HTML, CSS, and JS files in the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
